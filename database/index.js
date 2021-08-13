ea8b8976cdf0d1ed138d6fb5b271e64d1358ba3d
Mandolin Daze <renormalize@gmail.com>
2021-08-13 09:46:55 -0700

    Using clear db

--- database/index.js
+++ database/index.js
@@ -2,12 +2,13 @@
 
 const { Sequelize } = require("sequelize");
 
+let DB_HOST = process.env.CLEARDB_DATABASE_URL;
 const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
   process.env.DB_PASS,
   {
-    host: process.env.DB_HOST,
+    host: DB_HOST,
     dialect: "mysql",
   }
 );
